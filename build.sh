e2b5a780924e54a65cc2070f11a59bd705fa4bbe
Burman Vanesa Yanina <84544626+vaneburman@users.noreply.github.com>
2025-07-05 17:57:40 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
+
+# Instala las dependencias del requirements.txt
 pip install -r requirements.txt
-curl -o prometheus.proto https://raw.githubusercontent.com/prometheus/prometheus/main/prompb/prometheus.proto
+
+# Descarga el archivo de definición de Prometheus, siguiendo redirecciones (-L)
+echo "Descargando prometheus.proto..."
+curl -L -o prometheus.proto https://raw.githubusercontent.com/prometheus/prometheus/main/prompb/prometheus.proto
+
+# Genera el archivo python prometheus_pb2.py
+echo "Generando prometheus_pb2.py..."
 python -m grpc_tools.protoc -I=. --python_out=. prometheus.proto
+
+echo "Build finalizado."
+
